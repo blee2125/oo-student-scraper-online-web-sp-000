d04b666622e6a3557c08f599ce4548fdcbeb53d3
favorable-device-5158 <blee2125@gmail.com>
2020-01-16 14:22:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,24 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_html= Nokogiri::HTML(open(index_url))
+    students= []
+    index_html.css("div.roster-cards-container").each do |student_card|
+      student_card.css(".student-card a").each do |student|
+        student_profile_url= "#{student.attr('href')}"
+        student_location= student.css(".student-location").text
+        student_name= student.css(".student-name").text
+        students << {name: student_name, location: student_location, profile_url: student_profile_url}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_html= Nokogiri::HTML(open(profile_url))
+    student= {}
+
+    student
   end
 
 end
-
